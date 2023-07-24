eefedd55e7a6bb68aa7903032d5c718a4fb40591
Jayandra Thapa <jayandrathapa5518@gmail.com>
2023-07-24 11:01:35 +0545

    Navbar Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <h2>Gurkha Intern Management App</h2>
     </div>
   );
