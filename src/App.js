a9997b09b6b08d86cba4254e0492c1310a62c617
Jayandra Thapa <jayandrathapa5518@gmail.com>
2023-07-27 17:21:13 +0545

    Create done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
+import InternListing from './components/InternListing';
+import InternCreate from './components/InternCreate';
+import InternEdit from './components/InternEdit';
+import InternInfo from './components/InternInfo';
+
+import Home from './components/Home';
 import Intern from './components/Intern';
 import Navbar from './components/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <Navbar search={false}/>
-      {/* <h2>Gurkha Intern Management App</h2> */}
-      <Intern />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Home />} /> 
+          <Route exact path='/interndetails' element={<Intern />}></Route>
+          <Route exact path='/internlist' element={<InternListing />}></Route>
+          <Route exact path='/internlist/interncreate' element={<InternCreate />}></Route>
+          <Route exact path='/internlist/interninfo/:empid' element={<InternInfo />}></Route>
+          <Route exact path='/internlist/internedit/:empid' element={<InternEdit />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
