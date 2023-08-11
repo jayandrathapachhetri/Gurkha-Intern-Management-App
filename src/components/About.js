e352ba5f503c96b8c2a560490df61c2ef325cca9
Jayandra Thapa <jayandrathapa5518@gmail.com>
2023-08-11 13:24:58 +0545

    make navbar responsive

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,11 @@ export default function About() {
     <>
       <Navbar />
       <div className="container my-4">
-        <div className="row featurette d-flex justify-content-center align-items-center mb-3">
-          <div className="col-md-7">
+        <div className="row featurette align-items-center mb-5">
+          <div className="col-lg-5">
+            <img className="img-fluid rounded" src="/Images/about-1.jpg" alt="" />
+          </div>
+          <div className="col-lg-7">
             <h2 className="featurette-heading">
               Intern Details (Create, Read, Update, Delete)
             </h2>
@@ -15,12 +18,12 @@ export default function About() {
               This feature allows the admin to easily manage and maintain detailed profiles of all the interns. The app provides an intuitive user interface, enabling the admin to create new intern profiles by capturing essential information such as name, contact details, position, and start date. It also allows the admin to view, update, or delete intern records whenever necessary. By providing this CRUD functionality, the app ensures that intern data is always up-to-date and easily accessible.
             </p>
           </div>
-          <div className="col-md-5">
-            <img className="img-fluid" src="/Images/about-1.jpg" alt="" />
-          </div>
         </div>
-        <div className="row featurette d-flex justify-content-center align-items-center mb-3">
-          <div className="col-md-7 order-md-2">
+        <div className="row featurette align-items-center mb-5">
+          <div className="col-lg-5 order-lg-2">
+            <img className="img-fluid rounded" src="/Images/about-2.jpg" alt="" />
+          </div>
+          <div className="col-lg-7 order-lg-1">
             <h2 className="featurette-heading">
               Categorization and Filtering
             </h2>
@@ -28,21 +31,18 @@ export default function About() {
               The app offers a dynamic categorization system to classify interns based on their respective roles and expertise areas. Interns can be tagged under categories such as frontend, backend, flutter, QA, blockchain, full stack, etc. This categorization simplifies the process of locating specific groups of interns, allowing the admin to efficiently manage and monitor interns based on their roles and responsibilities.
             </p>
           </div>
-          <div className="col-md-5 order-md-1">
-            <img className="img-fluid" src="/Images/about-2.jpg" alt="" />
-          </div>
         </div>
-        <div className="row featurette d-flex justify-content-center align-items-center mb-3">
-          <div className="col-md-7 my-3">
+        <div className="row featurette align-items-center mb-5">
+          <div className="col-lg-5">
+            <img className="img-fluid rounded" src="/Images/about-3.jpg" alt="" />
+          </div>
+          <div className="col-lg-7">
             <h2 className="featurette-heading">
               Attendance System
             </h2>
             <p className="lead">
               One of the core functionalities of the app is the automated Attendance System. Interns can conveniently mark their daily attendance through the app, eliminating the need for manual attendance tracking. Each intern's attendance data is recorded in real-time and can be instantly viewed by the admin. The attendance system helps ensure punctuality, provides transparency, and enables the admin to monitor intern attendance effectively.
             </p>
-          </div>
-          <div className="col-md-5">
-            <img className="img-fluid" src="/Images/about-3.jpg" alt="" />
           </div>
         </div>
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate, NavLink , Link} from 'react-router-dom';
+import { useNavigate, NavLink, Link } from 'react-router-dom';
 
 function Navbar() {
   const user = JSON.parse(localStorage.getItem("user-info"));
@@ -9,159 +9,74 @@ function Navbar() {
   function logOut() {
     localStorage.removeItem('user-info');
     navigate("/");
-    console.log(" user logout")
+    console.log(" user logout");
     // localStorage.clear();
-    
   }
 
   return (
     <>
-        <nav className="navbar navbar-expand-lg navbar-dark  nav-pills sticky-top" style={{ backgroundColor:'#3f3d56' }}>
-          <div className="container-fluid">
-            <Link className="navbar-brand" to="/">
-              <img src="/favicon-32x32.png" alt="Logo" width="35" height="30" className="d-inline-block align-text-top mx-2"/>
-              Gurkha Technology
-            </Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse text-start" id="navbarSupportedContent">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                {localStorage.getItem("user-info") ? (
-                  <>
-                    <li className="nav-item mx-1">
-                      <NavLink className="nav-link" activeClassName="active" exact to="/">
-                        Home
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" activeClassName="active" to="/About">
-                        About
-                      </NavLink>
-                    </li>
-                    <li className="nav-item mx-1">
-                      <NavLink className="nav-link" activeClassName="active" to="/interndetails">
-                        Intern Details
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" activeClassName="active" to="/attendance">
-                        Attendance
-                      </NavLink>
-                    </li>
-                    <li className="nav-item mx-1">
-                      <NavLink className="nav-link" activeClassName="active" to="/internlist">
-                        Intern DashBoard
-                      </NavLink>
-                    </li>
-                  </>
-                ) : (
-                  <>
-                    <li className="nav-item">
-                    <NavLink className="nav-link mx-1" to="/register">Register</NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link mx-1" to="/login">Log in</NavLink>
-                    </li>
-                  </>
-                )}
-              </ul>
-            </div>
-          </div>
-          {localStorage.getItem("user-info") ? (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark nav-pills sticky-top">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            <img src="/favicon-32x32.png" alt="Logo" width="35" height="30" className="d-inline-block align-text-top mx-2" />
+            Gurkha Technology
+          </Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              {localStorage.getItem("user-info") ? (
+                <>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" activeClassName="active" exact to="/">
+                      Home
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" activeClassName="active" to="/About">
+                      About
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" activeClassName="active" to="/interndetails">
+                      Intern Details
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" activeClassName="active" to="/attendance">
+                      Attendance
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" activeClassName="active" to="/internlist">
+                      Intern Dashboard
+                    </NavLink>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/register">Register</NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className="nav-link" to="/login">Log in</NavLink>
+                  </li>
+                </>
+              )}
+            </ul>
+            {localStorage.getItem("user-info") ? (
+              <ul className="navbar-nav mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <NavLink  className="nav-link mx-3 bg-danger" onClick={logOut} >Logout</NavLink>
-                  </li>  
-             </ul>
-          ) : null} 
-        </nav>
+                  <button className="nav-link btn btn-danger" onClick={logOut}>Logout</button>
+                </li>
+              </ul>
+            ) : null}
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
 
 export default Navbar;
-
-// import React from 'react';
-// import { useNavigate, NavLink } from 'react-router-dom';
-// import { NavDropdown } from "react-bootstrap";
-
-// function Navbar() {
-//   const user = JSON.parse(localStorage.getItem("user-info"));
-//   console.warn(user);
-
-//   const navigate = useNavigate();
-//   function logout() {
-//     localStorage.removeItem('user-info');
-//     // localStorage.clear();
-//     navigate("/login");
-//   }
-
-//   return (
-//     <>
-//       <div className="container-fluid">
-//         <nav className="navbar navbar-expand-lg navbar-light bg-light nav-pills" style={{ backgroundColor:'#e3f2fd' }}>
-//           <div className="container-fluid">
-//             <a className="navbar-brand" href="/">
-//               <img src="/favicon-32x32.png" alt="Logo" width="35" height="30" className="d-inline-block align-text-top mx-2"/>
-//               Gurkha Technology
-//             </a>
-//             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-//               <span className="navbar-toggler-icon"></span>
-//             </button>
-//             <div className="collapse navbar-collapse text-start" id="navbarSupportedContent">
-//               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-//                 {localStorage.getItem("user-info") ? (
-//                   <>
-//                     <li className="nav-item mx-1">
-//                       <NavLink className="nav-link" activeClassName="active" exact to="/">
-//                         Home
-//                       </NavLink>
-//                     </li>
-//                     <li className="nav-item">
-//                       <NavLink className="nav-link" activeClassName="active" to="/About">
-//                         About
-//                       </NavLink>
-//                     </li>
-//                     <li className="nav-item mx-1">
-//                       <NavLink className="nav-link" activeClassName="active" to="/interndetails">
-//                         Intern Details
-//                       </NavLink>
-//                     </li>
-//                     <li className="nav-item">
-//                       <NavLink className="nav-link" activeClassName="active" to="/attadance">
-//                         Attadance
-//                       </NavLink>
-//                     </li>
-//                     <li className="nav-item mx-1">
-//                       <NavLink className="nav-link" activeClassName="active" to="/internlist">
-//                         Intern DashBoard
-//                       </NavLink>
-//                     </li>
-//                   </>
-//                 ) : (
-//                   <>
-//                     <li className="nav-item">
-//                       {/* <Link className="nav-link mx-1" to="/register">Register</Link> */}
-//                     </li>
-//                     <li className="nav-item">
-//                       <NavLink className="nav-link mx-1" to="/login">Log in</NavLink>
-//                     </li>
-//                   </>
-//                 )}
-//               </ul>
-//             </div>
-//           </div>
-//           {localStorage.getItem("user-info") ? (
-//             <NavDropdown title={user && user.name} style={{ backgroundColor: "gray", borderRadius: "5px" }}>
-//               <NavDropdown.Item onClick={logout}>logout</NavDropdown.Item>
-//               <NavDropdown.Item>Profile</NavDropdown.Item>
-//             </NavDropdown>
-//           ) : null} 
-//         </nav>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default Navbar;
