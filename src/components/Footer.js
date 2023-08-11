ba8443560abf30347c6af6238bdbebd97b67ea93
Jayandra Thapa <jayandrathapa5518@gmail.com>
2023-08-11 13:38:39 +0545

    make responsive footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,40 +1,30 @@
-import React from 'react'
-// import Navbar from './Navbar'
-
-let footerStyle = {
-  
-    position: "fixed",
-    left: "0",
-    bottom: "0",
-    width: "100%"
-  }
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-   
-   <>
-  <div className='container '>
-    <footer className="container" style={{footerStyle}}>
-      <hr className='featurette-divider' />
-      <p className="text-end"><a className="style-none text-decoration-none text-dark" href="#"> <i className="bi bi-arrow-up-circle-fill h3"></i></a></p>
-      <div className='row d-flex justify-content-between align-items-center'>
-        <p className='text-start fw-bold col-md-6 col-lg-3'>© 2023 Gurkhatech Company Inc.</p>
-        <div className='col-md-6 col-lg-3 d-flex justify-content-end mb-3'>
-          <a href="#"><i className="bi bi-twitter text-dark mx-1"></i></a>
-          <a href="#"><i className="bi bi-facebook text-dark mx-1"></i></a>
-          <a href="#"><i className="bi bi-linkedin text-dark mx-1"></i></a>
-          <a href="#"><i className="bi bi-instagram text-dark mx-1"></i></a>
+    <footer className="bg-light text-dark py-4">
+      <div className="container">
+        <hr className="featurette-divider" />
+        <p className="text-end mb-2">
+          <a className="text-dark" href="#">
+            <i className="bi bi-arrow-up-circle-fill h3"></i>
+          </a>
+        </p>
+        <div className="row d-flex justify-content-between align-items-center">
+          <p className="text-center text-md-start fw-bold col-md-6 col-lg-3 mb-md-0">
+            © 2023 Gurkhatech Company Inc.
+          </p>
+          <div className="col-md-6 col-lg-3 d-flex justify-content-center justify-content-md-end mb-3">
+            <Link to="#"><i className="bi bi-twitter text-dark mx-2"></i></Link>
+            <Link to="#"><i className="bi bi-facebook text-dark mx-2"></i></Link>
+            <Link to="#"><i className="bi bi-linkedin text-dark mx-2"></i></Link>
+            <Link to="#"><i className="bi bi-instagram text-dark mx-2"></i></Link>
+          </div>
         </div>
       </div>
     </footer>
-  </div>
-</>
-
-    
-    
-  ) 
+  );
 }
 
-export default Footer
-
-
+export default Footer;
