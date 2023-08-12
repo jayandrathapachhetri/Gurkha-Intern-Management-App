771a8d21cb559af72d65f16593c0332784e2deaf
Jayandra Thapa <jayandrathapa5518@gmail.com>
2023-08-12 11:19:56 +0545

    Home About RW done

--- src/components/About.js
+++ src/components/About.js
@@ -5,44 +5,79 @@ export default function About() {
   return (
     <>
       <Navbar />
-      <div className="container my-4">
-        <div className="row featurette align-items-center mb-5">
-          <div className="col-lg-5">
-            <img className="img-fluid rounded" src="/Images/about-1.jpg" alt="" />
-          </div>
-          <div className="col-lg-7">
-            <h2 className="featurette-heading">
-              Intern Details (Create, Read, Update, Delete)
-            </h2>
-            <p className="lead">
-              This feature allows the admin to easily manage and maintain detailed profiles of all the interns. The app provides an intuitive user interface, enabling the admin to create new intern profiles by capturing essential information such as name, contact details, position, and start date. It also allows the admin to view, update, or delete intern records whenever necessary. By providing this CRUD functionality, the app ensures that intern data is always up-to-date and easily accessible.
-            </p>
-          </div>
-        </div>
-        <div className="row featurette align-items-center mb-5">
-          <div className="col-lg-5 order-lg-2">
-            <img className="img-fluid rounded" src="/Images/about-2.jpg" alt="" />
-          </div>
-          <div className="col-lg-7 order-lg-1">
-            <h2 className="featurette-heading">
-              Categorization and Filtering
-            </h2>
-            <p className="lead">
-              The app offers a dynamic categorization system to classify interns based on their respective roles and expertise areas. Interns can be tagged under categories such as frontend, backend, flutter, QA, blockchain, full stack, etc. This categorization simplifies the process of locating specific groups of interns, allowing the admin to efficiently manage and monitor interns based on their roles and responsibilities.
-            </p>
-          </div>
-        </div>
-        <div className="row featurette align-items-center mb-5">
-          <div className="col-lg-5">
-            <img className="img-fluid rounded" src="/Images/about-3.jpg" alt="" />
-          </div>
-          <div className="col-lg-7">
-            <h2 className="featurette-heading">
-              Attendance System
-            </h2>
-            <p className="lead">
-              One of the core functionalities of the app is the automated Attendance System. Interns can conveniently mark their daily attendance through the app, eliminating the need for manual attendance tracking. Each intern's attendance data is recorded in real-time and can be instantly viewed by the admin. The attendance system helps ensure punctuality, provides transparency, and enables the admin to monitor intern attendance effectively.
-            </p>
+      <div className="container mt-5">
+        <div className="row col">
+          <div className="container mt-4">
+            <div className="row featurette align-items-center">
+              <div className="col-lg-5">
+                <img
+                  className="img-fluid rounded"
+                  src="/Images/about-1.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="col-lg-7">
+                <h2 className="featurette-heading">
+                  Intern Details (Create, Read, Update, Delete)
+                </h2>
+                <p className="lead">
+                  This feature allows the admin to easily manage and maintain
+                  detailed profiles of all the interns. The app provides an
+                  intuitive user interface, enabling the admin to create new
+                  intern profiles by capturing essential information such as
+                  name, contact details, position, and start date. It also
+                  allows the admin to view, update, or delete intern records
+                  whenever necessary. By providing this CRUD functionality, the
+                  app ensures that intern data is always up-to-date and easily
+                  accessible.
+                </p>
+              </div>
+            </div>
+            <div className="row featurette align-items-center mb-5">
+              <div className="col-lg-5 order-lg-2">
+                <img
+                  className="img-fluid rounded"
+                  src="/Images/about-2.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="col-lg-7 order-lg-1">
+                <h2 className="featurette-heading">
+                  Categorization and Filtering
+                </h2>
+                <p className="lead">
+                  The app offers a dynamic categorization system to classify
+                  interns based on their respective roles and expertise areas.
+                  Interns can be tagged under categories such as frontend,
+                  backend, flutter, QA, blockchain, full stack, etc. This
+                  categorization simplifies the process of locating specific
+                  groups of interns, allowing the admin to efficiently manage
+                  and monitor interns based on their roles and responsibilities.
+                </p>
+              </div>
+            </div>
+            <div className="row featurette align-items-center mb-5">
+              <div className="col-lg-5">
+                <img
+                  className="img-fluid rounded"
+                  src="/Images/about-3.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="col-lg-7">
+                <h2 className="featurette-heading">Attendance System</h2>
+                <p className="lead">
+                  One of the core functionalities of the app is the automated
+                  Attendance System. Interns can conveniently mark their daily
+                  attendance through the app, eliminating the need for manual
+                  attendance tracking. Each intern's attendance data is recorded
+                  in real-time and can be instantly viewed by the admin. The
+                  attendance system helps ensure punctuality, provides
+                  transparency, and enables the admin to monitor intern
+                  attendance effectively.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,67 +7,93 @@ function Home() {
   return (
     <>
       <Navbar />
-
-      <section className="bg-light text-dark p-5 p-lg-0 pt-lg-5 text-center text-sm-start">
-        <div className="container">
-          <div className="d-sm-flex align-items-center justify-content-between">
-            <div className="mb-4">
-              <h1 className="display-3">
-                Gurkha Intern Management App
-              </h1>
-              <p className="lead my-4">
-                A responsive web application designed to streamline administrative activities for interns. This app offers essential features to enhance efficiency and improve management.
-              </p>
-              <Link
-                className="btn btn-primary btn-lg"
-                to="/about"
-                role="button"
-              >
-                Learn more
-              </Link>
+      <div className="container mt-5">
+        <div className="row col">
+          <section className="bg-light text-dark p-5 p-lg-0 pt-lg-5 text-center text-sm-start ">
+            <div className="container mt-4">
+              <div className="d-sm-flex align-items-center justify-content-between">
+                <div className="mb-4">
+                  <h1 className="display-3">Gurkha Intern Management App</h1>
+                  <p className="lead my-4">
+                    A responsive web application designed to streamline
+                    administrative activities for interns. This app offers
+                    essential features to enhance efficiency and improve
+                    management.
+                  </p>
+                  <Link
+                    className="btn btn-primary btn-lg"
+                    to="/about"
+                    role="button"
+                  >
+                    Learn more
+                  </Link>
+                </div>
+                <img
+                  className="img-fluid w-50 d-none d-sm-block"
+                  src="Images/humanresource.svg"
+                  alt=""
+                />
+              </div>
             </div>
-            <img
-              className="img-fluid w-50 d-none d-sm-block"
-              src="Images/humanresource.svg"
-              alt=""
-            />
-          </div>
-        </div>
-      </section>
+            <h2 className="text-center my-5">
+              Become a part of
+              <span className="text-warning"> Gurkha Techology </span>
+            </h2>
+          </section>
 
-      <section className="bg-light text-dark p-5 p-lg-0 pt-lg-5 text-center text-sm-start">
-        <div className="container d-none d-sm-block">
-          <Carousel fade>
-            <Carousel.Item>
-              <img src="Images/home1.jpg" className="d-block w-100" alt="..." />
-              <Carousel.Caption>
-                <h2>Intern Details</h2>
-                <p className="lead">
-                  Manage intern profiles with ease. Create, read, update, and delete their information. The app provides a user-friendly interface to handle intern data effectively.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img src="Images/home2.jpg" className="d-block w-100" alt="..." />
-              <Carousel.Caption>
-                <h2>Categorization and Filtering</h2>
-                <p className="lead">
-                  Categorize interns based on roles and use filters to access specific groups quickly. Simplify tracking and management with detailed information.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img src="Images/home3.jpg" className="d-block w-100" alt="..." />
-              <Carousel.Caption>
-                <h2>Attendance System</h2>
-                <p className="lead">
-                  Automate attendance tracking for interns. Admins can mark daily attendance and monitor records in real-time, improving punctuality and transparency.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
+          <section className="bg-light text-dark p-5 p-lg-0 pt-lg-5 text-center text-sm-start">
+            <div className="container d-none d-sm-block">
+              <Carousel fade>
+                <Carousel.Item>
+                  <img
+                    src="Images/home1.jpg"
+                    className="d-block w-100"
+                    alt="..."
+                  />
+                  <Carousel.Caption>
+                    <h2>Intern Details</h2>
+                    <p className="lead fw-normal">
+                      Manage intern profiles with ease. Create, read, update,
+                      and delete their information. The app provides a
+                      user-friendly interface to handle intern data effectively.
+                    </p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    src="Images/home2.jpg"
+                    className="d-block w-100"
+                    alt="..."
+                  />
+                  <Carousel.Caption>
+                    <h2>Categorization and Filtering</h2>
+                    <p className="lead fw-normal">
+                      Categorize interns based on roles and use filters to
+                      access specific groups quickly. Simplify tracking and
+                      management with detailed information.
+                    </p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    src="Images/home3.jpg"
+                    className="d-block w-100"
+                    alt="..."
+                  />
+                  <Carousel.Caption>
+                    <h2>Attendance System</h2>
+                    <p className="lead fw-normal">
+                      Automate attendance tracking for interns. Admins can mark
+                      daily attendance and monitor records in real-time,
+                      improving punctuality and transparency.
+                    </p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              </Carousel>
+            </div>
+          </section>
         </div>
-      </section>
+      </div>
     </>
   );
 }
