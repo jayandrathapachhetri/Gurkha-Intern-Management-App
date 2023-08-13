7dc691ecffb88762097924e2dff5a5261da0df96
Jayandra Thapa <jayandrathapa5518@gmail.com>
2023-08-13 11:37:24 +0545

    Update the InternListing Page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,11 +21,11 @@ function Navbar() {
             <img src="/favicon-32x32.png" alt="Logo" width="35" height="30" className="d-inline-block align-text-top mx-2" />
             Gurkha Technology
           </Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <div className="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
               {localStorage.getItem("user-info") ? (
                 <>
                   <li className="nav-item mx-1">
@@ -64,7 +64,7 @@ function Navbar() {
                   </li>
                 </>
               )}
-            </ul>
+            </ul >
             {localStorage.getItem("user-info") ? (
               <ul className="navbar-nav mb-2 mb-lg-0">
                 <li className="nav-item">
